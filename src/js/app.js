f95fa3a0f7da5366d23d773ba6f964558d531a68
harada <sthsoulful@gmail.com>
2015-07-09 14:46:49 +0900

    add basic todo app html(with jsx) and css

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,74 @@
-var React = require('react/addons');
-var util = require('./util.js');
+(function(){
+    var React = require('react/addons');
 
-React.render(
-    <h1>TODOs</h1>,
-    document.getElementById('todo-app')
-);
-util();
+    var TodoApp = React.createClass({
+        render: function(){
+            return (
+                <div className="container">
+                    <header>
+                        <h1>todos</h1>
+                    </header>
+                    <section className="main-area">
+                        <div className="todo-input-area">
+                            <input className="check-all-todos" type="checkbox" ></input>
+                            <input className="todo-input" type="text" placeholder="What needs to be done?"></input>
+                        </div>
+                        <ul className="todo-list">
+                            <li className="todo-list-item">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">aiueo</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
+                                </div>
+                            </li>
+                            <li className="todo-list-item completed">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">kakiku</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
+                                </div>
+                            </li>
+                            <li className="todo-list-item editing">
+                                <div className="todo-list-item-view-box">
+                                    <input className="todo-list-item-check" type="checkbox"></input>
+                                    <span className="todo-list-item-label">kakiku</span>
+                                    <button className="todo-list-item-remove" type="button"></button>
+                                </div>
+                                <div className="todo-list-item-edit-box">
+                                    <input type="text"></input>
+                                </div>
+                            </li>
+                        </ul>
+                        <footer>
+                            <span>1 item left</span>
+                            <ul className="filter-list">
+                                <li className="filter-list-item">
+                                    <a href="#all" className="selected">All</a>
+                                </li>
+                                <li className="filter-list-item">
+                                    <a href="#active" >Active</a>
+                                </li>
+                                <li className="filter-list-item">
+                                    <a href="#completed" >Completed</a>
+                                </li>
+                            </ul>
+                            <button type="button" className="clear-completed">clear completed</button>
+                        </footer>
+                    </section>
+                </div>
+            );
+        }
+    });
+
+    React.render(
+        <TodoApp></TodoApp>,
+        document.getElementById('todo-app')
+    );
+})();
+
