3267b8007b1b0d19cba9352a7168a127bcfcb757
harada <sthsoulful@gmail.com>
2015-07-09 16:18:58 +0900

    extract and create TodoItem

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,39 @@
 (function(){
     var React = require('react/addons');
 
+    var TodoItem = React.createClass({
+        render: function(){
+            var todo = this.props.todo;
+            var completed = todo.status === 1;
+            return (
+                <li className={completed ? 'completed todo-list-item' : "todo-list-item" }>
+                    <div className="todo-list-item-view-box">
+                        <input className="todo-list-item-check" type="checkbox" checked={completed}></input>
+                        <span className="todo-list-item-label">{todo.label}</span>
+                        <button className="todo-list-item-remove" type="button"></button>
+                    </div>
+                    <div className="todo-list-item-edit-box">
+                        <input type="text"></input>
+                    </div>
+                </li>
+            );
+        }
+    });
+
     var TodoApp = React.createClass({
         getInitialState:function(){
             return{
                 todos:[
-                    {id:'_1',status:0,label:"call to mom."},
-                    {id:'_2',status:1,label:"walk the dog"},
-                    {id:'_3',status:0,label:"buy groceries for dinner"}
+                    {id:'_1',status:0, label:"call to mom."},
+                    {id:'_2',status:1, label:"walk the dog"},
+                    {id:'_3',status:0, label:"buy groceries for dinner"}
                 ]
             }
         },
         render: function(){
 
             var todoArray = this.state.todos.map(function(todo){
-                var completed = todo.status === 1;
-                return (
-                    <li className={completed ? 'completed todo-list-item' : "todo-list-item" }>
-                        <div className="todo-list-item-view-box">
-                            <input className="todo-list-item-check" type="checkbox" checked={completed}></input>
-                            <span className="todo-list-item-label">{todo.label}</span>
-                            <button className="todo-list-item-remove" type="button"></button>
-                        </div>
-                        <div className="todo-list-item-edit-box">
-                            <input type="text"></input>
-                        </div>
-                    </li>
-                );
+                return <TodoItem key={todo.id} todo={todo}></TodoItem>
             });
 
             return (
